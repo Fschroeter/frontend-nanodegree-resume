f9dfa50dc4a6d82dfe76db881931b332a2ad5b26
Frank Schröter <petemitchell2016@yahoo.com>
2018-02-20 11:37:43 -0800

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -30,16 +30,18 @@ var bio = {
         $("#header").append(formattedBioPic,formattedWelcomeMsg);
         // $("#header").append(formattedWelcomeMsg);
 
-        for(i in formattedContactInfo) {
-           $("#topContacts").append(formattedContactInfo[i]);
-           $("#footerContacts").append(formattedContactInfo[i]);
-        }
-        if(bio.skills.length > 0) {
+        //formattedContactInfo.forEach( function (contacts) { just repeats 4 times
+		//for(i in formattedContactInfo) { - no need for any loop - just top and footer
+           $("#topContacts").append(formattedContactInfo);
+           $("#footerContacts").append(formattedContactInfo);
+                
             $("#header").append(HTMLskillsStart);
-            for(i in bio.skills) {
-              $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
-            }
-        }
+		
+            bio.skills.forEach( function(skills) {
+				skill = HTMLskills.replace("%data%", skills)
+                $("#skills").append(skill);
+            });
+        
     }
 }
 
@@ -122,29 +124,42 @@ var work = {
 		"employer": "Business & Decision",
 		"title": "VP Reporting & Analytics",
 		"location": "Hamburg, Germany",
-		"dates": "February 2012 - Juli 2014",
-		"description": "• Hired and managed the consulting team for the German office.• Created the sales and SI manager role, providing leadership to guide the team through operational initiatives. • Project-managed the R&A sales and project, OKR setting process each quarter for 12+FTE and 3 TVCs. • Developed and introduced five vertical solutions and R&A products utilizing BusinessObjects 4.0 and QlikView. • Created and executed a multi-channel distribution strategy for B&D content marketing.",
+		"dates": "February 2012 - Juli 2014", // applied CR for better code visibility
+		"description": "• Hired and managed the consulting team for the German " +
+			"office.• Created the sales and SI manager role, providing leadership " +
+			"to guide the team through operational initiatives. • Project-managed " +
+			"the R&A sales and project, OKR setting process each quarter for 12+FTE " +
+			"and 3 TVCs. • Developed and introduced five vertical solutions and R&A " +
+			"products utilizing BusinessObjects 4.0 and QlikView. • Created and " +
+			"executed a multi-channel distribution strategy for B&D content marketing.",
      	},
         {
 		"employer": "Fidus",
 		"title": "Software Engineer",
 		"location": "Zurich, Switzerland",
 		"dates": "June 2011 - January 2012",
-		"description": "• BusinessObjects internal roll out project for Kuehne & Nagel, Schindellegi, CH"
+		"description": "• BusinessObjects internal roll out project for Kuehne & " +
+			"Nagel, Schindellegi, CH"
      	},
         {
 		"employer": "Kuehne & Nagel",
 		"title": "BI Rollout Consultant",
 		"location": "New York,NY",
-		"dates": "April 2010 - Mai 2011",
-		"description": "• Business Intelligence strategic consultant: strategic BI projects manager • Business Objects Manager: internal roll-out, training, content management, established BOCE and support intranet • Conducted enterprise-wide audits to accomplish optimization and implementation • Pre-sales meetings, user trainings, solution design"
+		"dates": "April 2010 - Mai 2011", // applied CR for better code visibility
+		"description": "• Business Intelligence strategic consultant: strategic BI projects manager " +
+			"• Business Objects Manager: internal roll-out, training, content management, " +
+			"established BOCE and support intranet • Conducted enterprise-wide audits to accomplish " + "optimization and implementation • Pre-sales meetings, user trainings, solution design"
       	},
         {
 		"employer": "Actuate Corporation",
 		"title": "Sr. Market Dev Manager",
 		"location": "San Mateo, CA",
 		"dates": "January 2008 - January 2009",
-		"description": "• Created competitive intelligence repository and executed CI campaigns. • Oversaw programs, sales tools, contracts and agreements for lead generation process. • $750k budget contributed to $4.5m revenue in first 2 quarters. • Customer engagement, marketing collateral, user groups and call center management."
+			// applied CR for better code visibility
+		"description": "• Created competitive intelligence repository and executed CI campaigns. " +
+			"• Oversaw programs, sales tools, contracts and agreements for lead generation " +
+			"process. • $750k budget contributed to $4.5m revenue in first 2 quarters. • " +
+			"Customer engagement, marketing collateral, user groups and call center management."
      	},
         {
 		"employer": "SAP",
@@ -182,19 +197,19 @@ var projects = {
 	"projects": [
 		{
 			"title": "Mars robot",
-			"datesWorked": "June 2017 - July 2017",
+			"dates": "June 2017 - July 2017",
 			"description": "Programmed a RC rover for the Mars mission as part of Udacity's robotics nanadegree program",
 			"images": ["images/rover.jpg"],
 		},
         {
 			"title": "Serial Manipulator - Pick and Place",
-			"datesWorked": "October 2017 - November 2017",
+			"dates": "October 2017 - November 2017",
 			"description": "Programmed a RC rover for the Mars mission as part of Udacity's robotics nanadegree program",
 			"images": ["images/manipulator.jpg"],
 		},
         {
-			"title": "Online Portfolio",
-			"datesWorked": "July 2017",
+			"title": "Online Portfolio", 
+			"dates": "July 2017",
 			"description": "One of the 9 projects at the nanodegree course 'Front End Web Develoment' is the creation of an online portfolio showcasing our project in Github and in an UI",
 			"images": ["images/portfolio.jpg"],
 		}
@@ -206,7 +221,7 @@ var projects = {
 		  //for(i in projects.projects) {
 		  projects.projects.forEach(function(project) {	
 			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.title).replace("#", project.url);
-			var formattedProjectDates = HTMLprojectDates.replace("%data%", project.datesWorked);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", project.dates);
 			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.description);
 
 			$(".project-entry:last").append(formattedProjectTitle);
